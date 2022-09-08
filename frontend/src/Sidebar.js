e6de4c0655e98d1937099a1de7b90b49af831179
rohangreddy <rohanreddy34@gmail.com>
2022-09-08 19:41:45 -0400

    Sidebar styling

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Sidebar.css";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
 
 /* rfce using VSC es7 extension to create functional component */
 function Sidebar() {
@@ -11,8 +13,22 @@ function Sidebar() {
             <ExpandMoreIcon />
         </div>
 
+        <div className="sidebar__channels">
+            <div className="sidebar__channelsHeader">
+                <div className="sidebar__header">
+                    <ExpandMoreIcon />
+                    <h4>Text Channels</h4>
+                </div>
+                {/* addChannel will allow for adding channel functionality */}
+                <AddIcon className="sidebar__addChannel" />
+            </div>
 
-
+            <div className="sidebar__channelsList">
+            {/* Component that will allow for adding of channels */}
+            <SidebarChannel />
+            </div>
+            
+        </div>
     </div>
   )
 }
---
+++ frontend/src/SidebarChannel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./SidebarChannel.css";
+
+function SidebarChannel({id, channel}) {
+  return (
+    <div className="sidebarChannel">
+        <h4><span className='sidebarChannel__hash'>#</span>Bitcoin</h4>
+
+    </div>
+  )
+}
+
+export default SidebarChannel
