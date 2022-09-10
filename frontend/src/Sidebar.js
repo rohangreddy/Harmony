d56d5763e00761ad9b8d2201ceaadc064b55343b
rohangreddy <rohanreddy34@gmail.com>
2022-09-09 20:46:31 -0400

    Sidebar design

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -3,6 +3,15 @@ import "./Sidebar.css";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import InfoIcon from '@mui/icons-material/Info';
+import CallIcon from '@mui/icons-material/Call';
+import Avatar from '@mui/material/Avatar';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+
 
 /* rfce using VSC es7 extension to create functional component */
 function Sidebar() {
@@ -28,6 +37,33 @@ function Sidebar() {
             <SidebarChannel />
             </div>
             
+        </div>
+        <div className="sidebar__voice">
+            <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large"/>
+            <div className="sidebar__voiceInfo">
+                <h3>Voice Connected</h3>
+                    <p>Stream</p>
+            </div>
+
+            <div className="sidebar__voiceIcons">
+                <InfoIcon />
+                <CallIcon />
+            </div>
+        </div>
+        
+        <div className="sidebar__profile">
+            <Avatar />
+            <div className="sidebar__profileInfo">
+                <h3>@satoshi</h3>
+                <p>#thisMyID</p>
+            </div>
+
+            <div className="sidebar__profileIcons">
+                <HeadphonesIcon />
+                <MicIcon />
+                <SettingsIcon />
+
+            </div>
         </div>
     </div>
   )
